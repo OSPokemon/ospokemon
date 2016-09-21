797a81ef77753c1796edc06ec3db7db44b037da9
Zach Taylor <kaikiwit@gmail.com>
2016-09-21 04:52:11 -0500

    Fix LogoutHandler
    now fires SessionExpire instead of AccountLogout

--- server/logout.go
+++ server/logout.go
@@ -1,13 +1,12 @@
 package server
 
 import (
-	"github.com/ospokemon/ospokemon/save"
 	"github.com/ospokemon/ospokemon/util"
 	"net/http"
 )
 
 var LogoutHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	if s := readsession(r); s != nil {
-		util.Event.Fire(save.EVNT_AccountLogout, s.Username)
+		util.Event.Fire(EVNT_SessionExpire, s, r)
 	}
 })
